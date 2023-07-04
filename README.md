23aa4e2c023aa098b235fc4c3c9cbe7c5309027c
kizzcross <kizzcrossoi@gmail.com>
2023-07-04 20:23:04 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,79 @@
+# Modbus Connection with Go and React
+
+This project demonstrates how to establish a Modbus connection using Go on the backend and React on the frontend. It receives three Modbus values and dynamically displays a color based on these values.
+
+## Prerequisites
+
+To run this project, you'll need the following software installed on your machine:
+
+- Go (v1.15 or above)
+- Node.js (v14 or above)
+- Yarn (v1.22 or above)
+
+## Installation
+
+### Backend (Go)
+
+1. Clone this repository to your local machine.
+2. Navigate to the `backend` directory.
+3. Run the following command to install the Go dependencies:
+
+```shell
+go mod download
+```
+
+4. Configure the Modbus connection details in the `config.go` file.
+5. Start the backend server with the following command:
+
+```shell
+go run main.go
+```
+
+The backend server should now be running on `http://localhost:8000`.
+
+### Frontend (React)
+
+1. Open a new terminal window.
+2. Navigate to the `frontend` directory.
+3. Run the following command to install the Node.js dependencies:
+
+```shell
+yarn install
+```
+
+4. Start the React development server with the following command:
+
+```shell
+yarn start
+```
+
+The React application should now be running on `http://localhost:3000`.
+
+## Configuration
+
+Before running the project, you need to configure the Modbus connection details. Follow the steps below:
+
+1. Open the `backend/config.go` file.
+2. Modify the `ModbusHost`, `ModbusPort`, `ModbusUnitID`, and `ModbusTimeout` variables to match your Modbus server configuration.
+
+## Usage
+
+1. Open your web browser and visit `http://localhost:3000`.
+2. You should see the application interface displaying a color box.
+3. The backend server will continuously read the Modbus values and send them to the frontend.
+4. The color of the box will update dynamically based on the values received.
+
+## Contributing
+
+Contributions are welcome! If you find a bug or want to add a new feature, please follow these steps:
+
+1. Fork this repository.
+2. Create a new branch: `git checkout -b my-new-branch`.
+3. Make your changes and commit them: `git commit -m 'Add new feature'`.
+4. Push to the branch: `git push origin my-new-branch`.
+5. Submit a pull request.
+
+## Acknowledgments
+
+- This project was inspired by the need to establish a Modbus connection and dynamically display colors based on received values.
+- We would like to express our gratitude to the open-source community for providing valuable libraries and resources.
---
+++ client/src/App.tsx
@@ -0,0 +1,88 @@
+import React, { useEffect, useState } from 'react';
+import LiquidBar from './components/bar';
+
+const LedButton = () => {
+  const [color, setColor] = useState('white');
+  const [firstValue, setFirstValue] = useState(0);
+  const [secondValue, setSecondValue] = useState(0);
+  const [thirdValue, setThirdValue] = useState(0);
+
+  useEffect(() => {
+    // Simulating API call
+    const fetchData = async () => {
+      try {
+        const response = await fetch("http://127.0.0.1:3000/modbus-data");
+        const data = await response.json();
+
+        // Extracting the values from the API response
+        const newFirstValue = data.Register0;
+        const newSecondValue = data.Register1;
+        const newThirdValue = data.Register2;
+
+        // Updating the color based on the values
+        setFirstValue(newFirstValue);
+        setSecondValue(newSecondValue);
+        setThirdValue(newThirdValue);
+        updateColor(newFirstValue, newSecondValue, thirdValue);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    // Fetching data from the API every 5 seconds
+    const interval = setInterval(fetchData, 1000);
+
+    // Cleaning up the interval on component unmount
+    return () => clearInterval(interval);
+  }, []);
+
+  const updateColor = (firstValue, secondValue, thirdValue) => {
+    let newColor = 'white';
+
+    // Adjusting color based on value ranges
+
+
+  function getColor(redValue, blueValue, greenValue) {
+  // Ensure redValue and blueValue are within the valid range (0-100)
+  redValue = Math.max(0, Math.min(100, redValue));
+  blueValue = Math.max(0, Math.min(100, blueValue));
+  greenValue = Math.max(0, Math.min(100, greenValue));
+
+
+  // Convert the RGB values to hexadecimal color representation
+  const redHex = Math.floor((redValue / 100) * 255).toString(16).padStart(2, '0');
+  const greenHex = Math.floor((greenValue / 100) * 255).toString(16).padStart(2, '0');
+  const blueHex = Math.floor((blueValue / 100) * 255).toString(16).padStart(2, '0');
+  // Return the color in the format "#RRGGBB"
+  return `#${redHex}${greenHex}${blueHex}`;
+}
+
+newColor = getColor(firstValue, secondValue, thirdValue);
+
+
+    setColor(newColor);
+  };
+
+  
+
+  return (
+    <div style={{display: 'flex', flexDirection: 'column-reverse', alignItems:"center"}}>
+    <div>
+      <LiquidBar value={firstValue} color={'red'} />
+      <LiquidBar value={secondValue} color={'blue'} />
+      <LiquidBar value={thirdValue} color={'green'} />
+    </div>
+    <div
+      style={{
+        margin: '20px',
+        width: '100px',
+        height: '100px',
+        borderRadius: '50%',
+        backgroundColor: color,
+      }}
+    ></div>
+    </div>
+  );
+};
+
+export default LedButton;
